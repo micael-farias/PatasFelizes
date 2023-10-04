be4cbdc918cccd5e49634c1c8bc91c2c2677867d
Pedro-Emanuel <pedroems.147@gmail.com>
2023-10-03 23:00:33 -0300

    Criando repositório

---
+++ src/app/Main.java
@@ -0,0 +1,32 @@
+package app;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/View.fxml"));
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("/view/view.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Patas Felizes");
+            primaryStage.getIcons().add(new Image("/images/patas.png"));
+            primaryStage.show();
+        } catch(IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/view/ViewController.java
@@ -0,0 +1,135 @@
+package view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+
+public class ViewController implements Initializable {
+    
+    private Button menuButtonActive;
+    @FXML
+    private Button menuButtonPets;
+    @FXML
+    private Button menuButtonFinancas;
+    @FXML
+    private Button menuButtonAgenda;
+    @FXML
+    private Button menuButtonEquipe;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+    private void toExitedStyle(Button button) {
+        button.getStyleClass().remove("menuButtonEntered");
+        button.getStyleClass().add("menuButtonExited");
+    }
+
+    private void toEnteredStyle(Button button) {
+        button.getStyleClass().remove("menuButtonExited");
+        button.getStyleClass().add("menuButtonEntered");
+    }
+
+    private void disableActive() {
+        if(menuButtonActive != null) {
+            getRectangle(menuButtonActive).setVisible(false);
+            // ...
+        }
+    }
+
+    private void setActive(Button button) {
+        disableActive();
+        menuButtonActive = button;
+        getRectangle(menuButtonActive).setVisible(true);
+    }
+
+    private Rectangle getRectangle(Button button) {
+        Pane pane = (Pane) button.getGraphic();
+        for(Node node : pane.getChildren()) {
+            if(node instanceof Rectangle rectangle) {
+                return rectangle;
+            }
+        }
+        return null;
+    }
+
+    @FXML
+    private void menuButtonPetsExited(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        toExitedStyle(button);
+    }
+
+    @FXML
+    private void menuButtonPetsEntered(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        toEnteredStyle(button);
+    }
+
+    @FXML
+    private void menuButtonPetsClicked(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        setActive(button);
+        
+    }
+
+    @FXML
+    private void menuButtonFinancasExited(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        toExitedStyle(button);
+    }
+
+    @FXML
+    private void menuButtonFinancasEntered(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        toEnteredStyle(button);
+    }
+
+    @FXML
+    private void menuButtonFinancasClicked(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        setActive(button);
+    }
+
+    @FXML
+    private void menuButtonAgendaExited(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        toExitedStyle(button);
+    }
+
+    @FXML
+    private void menuButtonAgendaEntered(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        toEnteredStyle(button);
+    }
+
+    @FXML
+    private void menuButtonAgendaClicked(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        setActive(button);
+    }
+
+    @FXML
+    private void menuButtonEquipeExited(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        toExitedStyle(button);
+    }
+
+    @FXML
+    private void menuButtonEquipeEntered(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        toEnteredStyle(button);
+    }
+
+    @FXML
+    private void menuButtonEquipeClicked(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        setActive(button);
+    }
+    
+}
