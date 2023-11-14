2cf7bbf4f5c53ccc6a53cb881afe208efcb895cc
grazi-rdl <graziellarodrigues41@gmail.com>
2023-11-14 20:57:53 -0300

    Criei o design da tela de cadastrar tarefas

---
+++ src/main/controllers/CadastrarTarefaController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.controllers;
+
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import main.interfaces.Inicializador;
+
+/**
+ *
+ * @author grazi
+ */
+public class CadastrarTarefaController implements Inicializador {
+
+    @Override
+    public void Inicializar(Pane contentFather, Stage primmaryStage, Pane blackShadow) {
+    }
+    
+}
